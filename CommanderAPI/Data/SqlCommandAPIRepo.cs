ecdb3365bdf81cf88b0a5b34b957794a45f1da29
Chad Lewis <thewickk@thewickk.com>
2021-01-23 17:43:35 -0500

    Modify Startup.cs add repository for SQL connection

---
+++ CommanderAPI/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using CommanderAPI.Models;
+using System.Collections.Generic;
+
+namespace CommanderAPI.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext _context;
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            _context = context ??
+                throw new ArgumentNullException(nameof(context));
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- CommanderAPI/Startup.cs
+++ CommanderAPI/Startup.cs
@@ -1,17 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using CommanderAPI.Data;
+using Npgsql;
 
 namespace CommanderAPI
 {
@@ -27,10 +22,17 @@ namespace CommanderAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString =
+                Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
             services.AddControllers();
 
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommanderAPI", Version = "v1" });
