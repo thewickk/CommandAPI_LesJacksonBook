c143a74a511f878b26c4535749275be64ccf56ad
Chad Lewis <thewickk@thewickk.com>
2021-01-23 11:48:01 -0500

    Install EF Packages and add DBContext

---
+++ CommanderAPI/Data/CommandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CommanderAPI.Models;
+
+namespace CommanderAPI.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
